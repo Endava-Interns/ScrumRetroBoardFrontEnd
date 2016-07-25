53822c33375747b38e5030c306c7c357dd8cfcae
Simona Stamenkova <simona_stamenkova@hotmail.com>
2016-07-25 10:58:52 +0200

    messages tests added

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -2,7 +2,7 @@ describe('Test', function () {
 
 	beforeEach(function () {
 		browser.get('/');
-	})
+	});
 
 	it('should open the user page', function () {
 
@@ -100,6 +100,174 @@ describe('Test', function () {
 		var user = element(by.id('Anonymous'));
 		expect(user.isPresent()).toBe(true);
 
+	});
+
+	it('should add a start message', function(){
+
+		var inputSession = element(by.id('sessionId'));
+		inputSession.sendKeys('testing123');
+		var joinSessionBtn = element(by.id('enterSession'));
+		joinSessionBtn.click();
+		browser.sleep(3000);
+		var input = element(by.id('username'));
+		input.sendKeys('');
+		var joinButton = element(by.id('joinSession'));
+		joinButton.click();
+		browser.sleep(4000);
+		var startButton = element(by.id('addStart'));
+		startButton.click();
+		browser.sleep(3000);
+		var msgInput = element(by.id('msgTextStart'));
+		msgInput.sendKeys('Hello');
+		var sendBtn = element(by.id('saveStart'));
+		sendBtn.click();
+		browser.sleep(4000);
+		var elm = element.all(by.repeater('startMessage in sessionVm.startMessages')).last().getText();
+		expect(elm).toEqual('Hello - (Anonymous)');
+	});
+
+	it('should edit a start message', function(){
+
+		var inputSession = element(by.id('sessionId'));
+		inputSession.sendKeys('testing123');
+		var joinSessionBtn = element(by.id('enterSession'));
+		joinSessionBtn.click();
+		browser.sleep(3000);
+		var input = element(by.id('username'));
+		input.sendKeys('');
+		var joinButton = element(by.id('joinSession'));
+		joinButton.click();
+		browser.sleep(4000);
+		var startButton = element(by.id('addStart'));
+		startButton.click();
+		browser.sleep(3000);
+		var msgInput = element(by.id('msgTextStart'));
+		msgInput.sendKeys('Hello');
+		var sendBtn = element(by.id('saveStart'));
+		sendBtn.click();
+		browser.sleep(4000);
+		element.all(by.repeater('startMessage in sessionVm.startMessages')).last().click();
+		browser.sleep(2000);
+		var text = element(by.id('textModify'));
+		text.clear();
+		text.sendKeys('Hello test edited');
+		var saveChanges = element(by.id('saveModify'));
+		saveChanges.click();
+		browser.sleep(4000);
+		var elm = element.all(by.repeater('startMessage in sessionVm.startMessages')).last().getText();
+		expect(elm).toEqual('Hello test edited - (Anonymous)');
+	});
+
+	it('should add a stop message', function(){
+
+		var inputSession = element(by.id('sessionId'));
+		inputSession.sendKeys('testing123');
+		var joinSessionBtn = element(by.id('enterSession'));
+		joinSessionBtn.click();
+		browser.sleep(3000);
+		var input = element(by.id('username'));
+		input.sendKeys('');
+		var joinButton = element(by.id('joinSession'));
+		joinButton.click();
+		browser.sleep(4000);
+		var startButton = element(by.id('addStop'));
+		startButton.click();
+		browser.sleep(3000);
+		var msgInput = element(by.id('msgTextStop'));
+		msgInput.sendKeys('Hello');
+		var sendBtn = element(by.id('saveStop'));
+		sendBtn.click();
+		browser.sleep(4000);
+		var elm = element.all(by.repeater('stopMessage in sessionVm.stopMessages')).last().getText();
+		expect(elm).toEqual('Hello - (Anonymous)');
+	});
+
+	it('should edit a stop message', function(){
+
+		var inputSession = element(by.id('sessionId'));
+		inputSession.sendKeys('testing123');
+		var joinSessionBtn = element(by.id('enterSession'));
+		joinSessionBtn.click();
+		browser.sleep(3000);
+		var input = element(by.id('username'));
+		input.sendKeys('');
+		var joinButton = element(by.id('joinSession'));
+		joinButton.click();
+		browser.sleep(4000);
+		var startButton = element(by.id('addStop'));
+		startButton.click();
+		browser.sleep(3000);
+		var msgInput = element(by.id('msgTextStop'));
+		msgInput.sendKeys('Hello');
+		var sendBtn = element(by.id('saveStop'));
+		sendBtn.click();
+		browser.sleep(4000);
+		element.all(by.repeater('stopMessage in sessionVm.stopMessages')).last().click();
+		browser.sleep(2000);
+		var text = element(by.id('textModify'));
+		text.clear();
+		text.sendKeys('Hello test edited');
+		var saveChanges = element(by.id('saveModify'));
+		saveChanges.click();
+		browser.sleep(4000);
+		var elm = element.all(by.repeater('stopMessage in sessionVm.stopMessages')).last().getText();
+		expect(elm).toEqual('Hello test edited - (Anonymous)');
+	});
+
+	it('should add a continue message', function(){
+
+		var inputSession = element(by.id('sessionId'));
+		inputSession.sendKeys('testing123');
+		var joinSessionBtn = element(by.id('enterSession'));
+		joinSessionBtn.click();
+		browser.sleep(3000);
+		var input = element(by.id('username'));
+		input.sendKeys('');
+		var joinButton = element(by.id('joinSession'));
+		joinButton.click();
+		browser.sleep(4000);
+		var startButton = element(by.id('addContinue'));
+		startButton.click();
+		browser.sleep(3000);
+		var msgInput = element(by.id('msgTextContinue'));
+		msgInput.sendKeys('Hello');
+		var sendBtn = element(by.id('saveContinue'));
+		sendBtn.click();
+		browser.sleep(4000);
+		var elm = element.all(by.repeater('continueMessage in sessionVm.continueMessages')).last().getText();
+		expect(elm).toEqual('Hello - (Anonymous)');
+	});
+
+	it('should edit a continue message', function(){
+
+		var inputSession = element(by.id('sessionId'));
+		inputSession.sendKeys('testing123');
+		var joinSessionBtn = element(by.id('enterSession'));
+		joinSessionBtn.click();
+		browser.sleep(3000);
+		var input = element(by.id('username'));
+		input.sendKeys('');
+		var joinButton = element(by.id('joinSession'));
+		joinButton.click();
+		browser.sleep(4000);
+		var startButton = element(by.id('addContinue'));
+		startButton.click();
+		browser.sleep(3000);
+		var msgInput = element(by.id('msgTextContinue'));
+		msgInput.sendKeys('Hello');
+		var sendBtn = element(by.id('saveContinue'));
+		sendBtn.click();
+		browser.sleep(4000);
+		element.all(by.repeater('continueMessage in sessionVm.continueMessages')).last().click();
+		browser.sleep(2000);
+		var text = element(by.id('textModify'));
+		text.clear();
+		text.sendKeys('Hello test edited');
+		var saveChanges = element(by.id('saveModify'));
+		saveChanges.click();
+		browser.sleep(4000);
+		var elm = element.all(by.repeater('continueMessage in sessionVm.continueMessages')).last().getText();
+		expect(elm).toEqual('Hello test edited - (Anonymous)');
 	});
 
 });
